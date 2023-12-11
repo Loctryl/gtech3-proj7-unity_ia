10308ca946bc27a1aa0b9db7f945e7dcd3691139
Cyriansavary <92294053+Cyriansavary@users.noreply.github.com>
2023-12-11 15:51:50 +0100

    fix git

---
+++ Assets/_Scripts/AbstractDungeonGenerator.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstractDungeonGenerator : MonoBehaviour
+{
+    [SerializeField]
+    protected TilemapVisulazer tilemapVisulazer = null;
+
+    [SerializeField]
+    protected Vector2Int startPosition = Vector2Int.zero;
+
+    public void GenerateDungeon()
+    {
+        tilemapVisulazer.Clear();
+        RunProceduralGeneration();
+    }
+
+    protected abstract void RunProceduralGeneration();
+
+
+}
