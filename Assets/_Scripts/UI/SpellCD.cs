df4c84ec625ae6b86d4d25c870ed2185e771970f
Cyriansavary <92294053+Cyriansavary@users.noreply.github.com>
2023-12-18 15:30:01 +0100

    lil fix

---
+++ Assets/_Scripts/UI/SpellCD.cs
@@ -0,0 +1,14 @@
+using Microsoft.Unity.VisualStudio.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellCD : MonoBehaviour
+{
+
+
+    [SerializeField]
+    private Image ImageCD;
+    [SerializeField]
+
+}
