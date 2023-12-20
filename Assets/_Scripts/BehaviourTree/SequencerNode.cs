1981f1e0adcd7f6905cdc811c397281565ae8b21
Lum0o_ <progpipart@gmail.com>
2023-12-20 17:15:13 +0100

    [UPDATE] update sequencerNode

--- Assets/_Scripts/BehaviourTree/SequencerNode.cs
+++ Assets/_Scripts/BehaviourTree/SequencerNode.cs
@@ -3,28 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SequencerNode : CompositeNode {
-	int current;
 
 	protected override void OnEnter() {
-		current = 0;
 	}
 
 	protected override void OnExit() {
 	}
 
-	protected override State OnUpdate() {
-		var child = children[current];
+	protected override State OnUpdate()
+	{
+		bool anyChildIsRunning = false;
 
-		switch (child.Update()) {
-			case State.Running:
-				return State.Running;
-			case State.Failure:
-				return State.Failure;
-			case State.Success:
-				current++;
-				break;
+		foreach (var child in children)
+		{
+			switch (child.Update())
+			{
+				case State.Running:
+					anyChildIsRunning = true;
+					break;
+				case State.Failure:
+					return State.Failure;
+				case State.Success:
+					break;
+			}
 		}
 
-		return current == children.Count ? State.Success : State.Running;
+		return anyChildIsRunning ? State.Running : State.Success;
 	}
 }
