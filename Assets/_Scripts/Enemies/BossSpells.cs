6b33a20f5914fb3c526c2f50713ab6470acc1037
Raphaël Mateescu <73947667+SmartNewt@users.noreply.github.com>
2023-12-21 18:55:26 +0100

    [fix] melee direction

--- Assets/_Scripts/Enemies/BossSpells.cs
+++ Assets/_Scripts/Enemies/BossSpells.cs
@@ -54,7 +54,7 @@ public class BossSpells : MonoBehaviour
     public void CastMeleeSL()
     {
         Vector3 direction =  player.transform.position - transform.position;
-        Instantiate(MeleeSingleTarget, transform.position, Quaternion.FromToRotation(Vector3.up, direction), transform);
+        Instantiate(MeleeSingleTarget, transform.position, Quaternion.FromToRotation(Vector3.forward, direction), transform);
     }
 
     public void CastTeleport()
